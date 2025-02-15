7c34bc6dbed5eeaa6b0dd54a7fe934f2e944f5a9
VAIBHAVSRIVASTAVA99 <srivastavavaibhav499@gmail.com>
2025-02-16 00:45:44 +0530

    Update Login.jsx

--- frontend/tourwebsite/src/pages/Login.jsx
+++ frontend/tourwebsite/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/user/signin`, {
+      const response = await fetch(https://go-travel-backend.onrender.com/user/signin, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
